22c7803d9a44fe4093054cdd0ea60f748be77a49
Jashaswee Jena <j.ishujena2000@gmail.com>
2020-09-01 04:05:30 +0530

    Add maps with marker

--- lib/maps.dart
+++ lib/maps.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Maps extends StatefulWidget {
@@ -10,11 +11,38 @@ class Maps extends StatefulWidget {
 
 class _MapsState extends State<Maps> {
   Completer<GoogleMapController> _controller = Completer();
+  Position currentLocation;
+  LatLng _center;
 
-  static const LatLng _center = const LatLng(45.521563, -122.677433);
+  Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+    getUserLocation();
+    super.initState();
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
+  }
+
+  Future<Position> locateUser() async {
+    return getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  }
+
+  getUserLocation() async {
+    currentLocation = await locateUser();
+    setState(() {
+      _center = LatLng(currentLocation.latitude, currentLocation.longitude);
+      _markers.add(Marker(
+        markerId: MarkerId("home"),
+        position: _center,
+        infoWindow: InfoWindow(
+          title: "Home!"
+        )
+      ));
+    });
+    print('center $_center');
   }
 
   @override
@@ -25,13 +53,16 @@ class _MapsState extends State<Maps> {
           title: Text('Maps Sample App'),
           backgroundColor: Colors.green[700],
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
+        body: _center == null
+            ? CircularProgressIndicator()
+            : GoogleMap(
+                onMapCreated: _onMapCreated,
+                markers: _markers,
+                initialCameraPosition: CameraPosition(
+                  target: _center,
+                  zoom: 11.0,
+                ),
+              ),
       ),
     );
   }
